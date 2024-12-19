debdde769cb71c4aadf919a23bc55c249f6d8534
SotaSill <sotasill@gmail.com>
2024-12-19 20:23:09 +0200

    gallery

--- src/js/galler.js
+++ src/js/galler.js
@@ -1,10 +1,22 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 
 const swiper = new Swiper('.swiper-container', {
+  modules: [Navigation, Pagination],
   slidesPerView: 1,
+  allowTouchMove: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  mousewheel: true,
 });
